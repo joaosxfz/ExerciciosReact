1c50bd1bc87fbbcd4b2acc30147128f249ab73a6
jvictorcs35@gmail.com <jvictorcs35@gmail.com>
2026-04-20 00:30:14 -0300

    PEGOU PORRA

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,7 +1,18 @@
-import './style.css'
+// components/Menu/index.jsx
+import { Link } from 'react-router-dom'
 
 function Menu() {
-    return (
-        <p>opa</p>
-    )
+  return (
+    <header>
+      <h1>Exercícios React</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/cadastro">Cadastro</Link>
+        <Link to="/sobre">Sobre</Link>
+      </nav>
+    </header>
+  )
 }
+
+export default Menu
