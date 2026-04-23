e39274cd4e63e4dd168b06bbf94ec39f1ce32985
joão souza <jvictorcs35@gmail.com>
2026-04-23 19:16:14 -0300

    oi

--- src/pages/Exercicios/Ex01/index.jsx
+++ src/pages/Exercicios/Ex01/index.jsx
@@ -2,7 +2,27 @@ import { useState } from 'react'
 
 function Ex01() {
     return (
-        <p>exercício 1</p>
+        <main>
+            <section>
+                <h2>Exercício 01</h2>
+                <p>1. Peça ao usuário o nome de um produto e o preço.
+                    Mostre uma mensagem dizendo quanto custa o produto.</p>
+            </section>
+            <section>
+                <form>
+                    <p>Nome Completo</p>
+                    <input type="text" />
+                    <p>CPF:</p>
+                    <input type="text" />
+                    <p>Telefone</p>
+                    <input type="text" />
+                    <p>Email</p>
+                    <input type="text" />
+                    <p>Senha</p>
+                    <input type="text" />
+                </form>
+            </section>
+        </main>
     )
 }
 
